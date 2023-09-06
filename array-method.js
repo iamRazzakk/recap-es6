e7f54647ade977b2da3981578763ea6eac139793
Md Abdur Razzak ~ <rakibt23p@gmail.com>
2023-09-06 22:29:05 +0600

    git commit

--- array-method.js
+++ array-method.js
@@ -8,14 +8,23 @@ const products =
 const brandName = products.map(product => product.brand);
 const prices = products.map(product => product.price)
 const productName = products.map(productNames => productNames.name)
-console.log(brandName, prices,productName);
-// const products = [
-//     { name: 'Laptop', price: 32000, brand: 'Lenovo', color: 'Silver' },
-//     { name: 'Phone', price: 700, brand: 'Iphone', color: 'Golden' },
-//     { name: 'Watch', price: 3000, brand: 'Casio', color: 'Yellow' },
-//     { name: 'Aunglass', price: 300, brand: 'Ribon', color: 'Blue' },
-//     { name: 'Camera', price: 9000, brand: 'Lenovo', color: 'Gray' },
-// ];
+// console.log(brandName, prices,productName);
 
-// const productName = products.map(product => product.name);
+
+const productsList = [
+    { name: 'Laptop', price: 32000, brand: 'Lenovo', color: 'Silver' },
+    { name: 'Phone', price: 700, brand: 'Iphone', color: 'Golden' },
+    { name: 'Watch', price: 3000, brand: 'Casio', color: 'Yellow' },
+    { name: 'Aunglass', price: 300, brand: 'Ribon', color: 'Blue' },
+    { name: 'Camera', price: 9000, brand: 'Lenovo', color: 'Gray' },
+];
+// const product = productsList.forEach(productlists => console.log(productlists.price))
+// console.log(product);
 // console.log(productName);
+
+// filter 
+const chips = productsList.filter(product => product.color == "Gray")
+const chipss = productsList.filter(product => product.color.includes("n"))
+// console.log(chips, chipss);
+const findNameOfN = productsList.find(product => product.name.includes("p"))
+console.log(findNameOfN);
---
+++ destructuring.js
@@ -0,0 +1,13 @@
+const employee = {
+    id: "vsCode",
+    desgination: "developer",
+    matchin: "Mac",
+    spacification: {
+        height: 6.4,
+        weight: 70,
+        address: "Chittagong",
+        dring: "Water"
+    }
+}
+const { matchin, id } = employee
+console.log(matchin,id);
