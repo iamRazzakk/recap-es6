de90b1d1aa2c88a3ec0c6fc00111bf34ce2eac41
Md Abdur Razzak ~ <rakibt23p@gmail.com>
2023-09-06 21:29:14 +0600

    git commit

---
+++ array-method.js
@@ -0,0 +1,21 @@
+const products =
+    [
+        { name: "leptop", price: 25000, brand: "leptop", color: "silver" },
+        { name: "Hp", price: 25000, brand: "Hp", color: "silver" },
+        { name: "MacAir", price: 2250000, brand: "Apple", color: "silver" },
+        { name: "Lenovo", price: 30000, brand: "lenovo", color: "silver" }
+    ]
+const brandName = products.map(product => product.brand);
+const prices = products.map(product => product.price)
+const productName = products.map(productNames => productNames.name)
+console.log(brandName, prices,productName);
+// const products = [
+//     { name: 'Laptop', price: 32000, brand: 'Lenovo', color: 'Silver' },
+//     { name: 'Phone', price: 700, brand: 'Iphone', color: 'Golden' },
+//     { name: 'Watch', price: 3000, brand: 'Casio', color: 'Yellow' },
+//     { name: 'Aunglass', price: 300, brand: 'Ribon', color: 'Blue' },
+//     { name: 'Camera', price: 9000, brand: 'Lenovo', color: 'Gray' },
+// ];
+
+// const productName = products.map(product => product.name);
+// console.log(productName);
